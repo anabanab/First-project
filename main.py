5d47a89dd10e1772a195118c03b1936529033e83
anabanab <m.a.anab@hotmail.com>
2024-09-25 15:52:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-print("hello-world")
 
-print("hej")
+# while True:
+#     print("\nVälj en datatyp för att få en förklaring:")
+#     print("1. Heltal (int)")
+#     print("2. Flyttal (float)")
+#     print("3. Sträng (string)")
+#     print("4. Boolesk värde (bool)")
+#     print("5. Lista (list)")
+#     print("6. Avsluta")
+
+#     val = input("Ange ditt val (1-6)")
+
+#     if val == "1":
+#         print("Heltal (int är ett helt nummer utan decimaler, som t.ex. 1, 100 eller -5)")
+        
+#     elif val == "2":
+#         print("Flyttal (float) är ett tal med decimaler, som t.ex. 1.5 eller 3.14.")
+#     elif val == "3":
+#         print("Sträng (string) är en sekvens av tecken omgiven av citattecken, som t.ex. 'hej'.")
+#     elif val == "4":
+#         print("Booleskt värde (bool) är antingen True(sant eller False(falskt).")
+#     elif val == "5":
+#         print("Lista (list) är en samling av värden, t.ex [1, 2, 3] eller ['a', 'b', 'c'].")
+#     elif val == "6":
+#         print("Avslutar programmet...")
+#         break 
+#     else:
+#         print("Ogiltigt val. Försök igen.")
+
+CPU = 35
+MEM = 30
+DISK = 60
+
+print(f"Variabel A = {CPU} Variabel B = {MEM} Variabel C = {DISK}")
+
+
+   
+
+
+
+
